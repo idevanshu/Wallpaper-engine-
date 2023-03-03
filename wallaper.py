858ce538b92152120a38ba2e4c187286ace2fee0
Devanshu <devanshupanigrahi2004@gmail.com>
2023-03-03 18:33:14 +0530

    First commit

---
+++ wallaper.py
@@ -0,0 +1,16 @@
+import requests
+import ctypes
+import random
+from PIL import Image
+import os
+
+keywords = ['car','coding','river','time']
+url = f"https://source.unsplash.com/random/1900x1080/?{random.choice(keywords)}"
+
+img = Image.open(requests.get(url, stream = True).raw)
+img.save('image.jpg')
+path = f'{os.getcwd()}\\image.jpg'
+
+
+SPI_SETDESKWALLPAPER = 20 
+ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path , 3)
