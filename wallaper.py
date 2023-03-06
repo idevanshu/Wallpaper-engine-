15f4403bea62a28360a7d1402a41366c4172fc02
Devanshu <devanshupanigrahi2004@gmail.com>
2023-03-06 13:17:20 +0530

    More optimised with the help of numpy & cv2

--- wallaper.py
+++ wallaper.py
@@ -1,16 +1,26 @@
 import requests
-import ctypes
+import cv2
 import random
-from PIL import Image
 import os
+import numpy as np
+import ctypes
 
-keywords = ['trees','wild','coding','code','setup','lamborghini','BMW','coder','nature']
-url = f"https://source.unsplash.com/random/3840x2160/?{random.choice(keywords)}"
+def get_img():
+    try:
+        keywords = ['beach','mercedes','coding','code','setup','lamborghini','BMW','coder','bugatti']
+        url = f"https://source.unsplash.com/random/3840x2160/?{random.choice(keywords)}"
+        response = requests.get(url)
+        image_array = response.content
+        image = cv2.imdecode(np.frombuffer(image_array, np.uint8), cv2.IMREAD_UNCHANGED)
+        cv2.imwrite("image.jpg", image)
+        return True
+    except Exception as e:
+        return False
 
-img = Image.open(requests.get(url, stream = True).raw)
-img.save('image.jpg')
-path = f'{os.getcwd()}\\image.jpg'
+def set_wallpaper():
+    path = f'{os.getcwd()}\\image.jpg'
+    SPI_SETDESKWALLPAPER = 20 
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path , 3)
 
-
-SPI_SETDESKWALLPAPER = 20 
-ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path , 3)
+if(get_img()):
+    set_wallpaper()
