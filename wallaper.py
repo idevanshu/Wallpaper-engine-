162e826f2932ff349c42f59e982f4365834c37ba
Devanshu Panigrahi <devanshupanigrahi2004@gmail.com>
2023-05-02 18:08:55 +0530

    Update wallaper.py

--- wallaper.py
+++ wallaper.py
@@ -3,15 +3,29 @@ import cv2
 import random
 import os
 import numpy as np
+import hashlib
 import ctypes
+import threading
+
+CACHE_DIR = "cache"
 
 def get_img():
     try:
-        keywords = ['beach','mercedes','coding','code','setup','lamborghini','BMW','coder','bugatti']
+        keywords = ['supercars','nature','hills']
+        keywords.sort()
         url = f"https://source.unsplash.com/random/3840x2160/?{random.choice(keywords)}"
-        response = requests.get(url)
-        image_array = response.content
-        image = cv2.imdecode(np.frombuffer(image_array, np.uint8), cv2.IMREAD_UNCHANGED)
+        # Generate a hash of the URL to use as the filename
+        filename = hashlib.md5(url.encode()).hexdigest() + ".jpg"
+        cache_path = os.path.join(CACHE_DIR, filename)
+        if os.path.exists(cache_path):
+            # If the image is already downloaded, load it from the cache
+            image = cv2.imread(cache_path)
+        else:
+            # Otherwise, download the image and save it to the cache
+            response = requests.get(url)
+            image_array = response.content
+            image = cv2.imdecode(np.frombuffer(image_array, np.uint8), cv2.IMREAD_UNCHANGED)
+            cv2.imwrite(cache_path, image)
         cv2.imwrite("image.jpg", image)
         return True
     except Exception as e:
@@ -22,5 +36,10 @@ def set_wallpaper():
     SPI_SETDESKWALLPAPER = 20 
     ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path , 3)
 
-if(get_img()):
-    set_wallpaper()
+if get_img():
+    # Download image in the background
+    t = threading.Thread(target=get_img)
+    t.start()
+
+# Set the wallpaper
+set_wallpaper()
